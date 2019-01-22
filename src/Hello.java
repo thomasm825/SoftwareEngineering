aa1f644cb427d7f52fb0c677ab9b46d05e100039
thomasm825 <thomasm825@ALB205-10.strose.edu>
2019-01-22 15:43:06 -0500

    Hello program

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Hello {
+	
+	public static void main(String[] args){
+		
+		Scanner x = new Scanner(System.in);
+		String name = x.nextLine();
+		System.out.println("The person running this program is:" + name);
+	}
+	
+}
