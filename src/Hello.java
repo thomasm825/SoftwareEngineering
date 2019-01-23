d27bae91a99c90345a3db236baade4085cb2490c
thomasm825 <thomasm825@strose.edu>
2019-01-23 16:43:24 -0500

    Hello.hs

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.*;
 
+//This program prints the users name who is running this program.
+
 public class Hello {
 	
 	public static void main(String[] args){
